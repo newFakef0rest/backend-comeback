3b63b8c10f72ee525a8e310563d974147c0c7082
newFakef0rest <fakef0rest69@gmail.com>
2026-09-21 19:51:09 +0500

    Add user administration and tests for it

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 const phonesRoute = require('./controllers/phones')
+const usersRoute = require('./controllers/users')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/persons', phonesRoute)
+app.use('/api/users', usersRoute)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
